595f4d9d31c0bc6d7ae1b88da0a1e166ed347e87
Ruben Almaraz <ruben.almaraz.arranz@everis.com>
2018-12-03 23:25:32 +0100

    03122018_VueltaAtras

--- PruebaVisualyGitK/GitkYEv/GitkYEv/Program.cs
+++ PruebaVisualyGitK/GitkYEv/GitkYEv/Program.cs
@@ -10,10 +10,7 @@ namespace GitkYEv
             for (int i = 0;i<100;i++){
                 Console.WriteLine(i);
             }
-            Console.WriteLine("Julio");
-            Console.WriteLine("Rama Ruben");
-            Console.WriteLine("Cambio1");
-            Console.WriteLine("Cambio2_Tomas");
+            Console.WriteLine("Reinicio de 0");
         }
     }
 }
