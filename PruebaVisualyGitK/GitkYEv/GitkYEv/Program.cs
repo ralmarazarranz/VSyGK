eda0b2acdff09d37ffd93722ed24867719231b86
Ruben Almaraz <ruben.almaraz.arranz@everis.com>
2018-12-03 22:03:07 +0100

    InicioProyecto03122018
    03122018 - Subimos a la rama master el proyecto con HolaMundo

---
+++ PruebaVisualyGitK/GitkYEv/GitkYEv/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitkYEv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0;i<100;i++){
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
